00934b8c9daebda06aabf53aac7b9cde847e41eb
Godfrey.N <65883099+Godfrey-cyber@users.noreply.github.com>
2024-06-11 18:12:33 +0000

    addition func

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ length = 12
 width = 8
 area = length * width
 print(area)
+def my_func(width, length):
+  return length * width
+
+area_2 = my_func(12, 7)
+area_3 = my_func(10, 5)
+
+print(area_2)
+print(area_3)
